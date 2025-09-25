ad5ad35a9254f004fc1dce393a8b6c5dc2a0f423
Mahmoud Aboelsoud <mahmoud.aboelsoud.p@gmail.com>
2025-09-25 22:55:52 +0300

    Fix ansible

--- deploy/docker/entrypoint.sh
+++ deploy/docker/entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 echo "Running entrypoint.sh ..."
 
-python3 ./deploy/license-verification/src/main.py
+sudo python3 ./deploy/license-verification/src/main.py
 exit_code=$?
 if [ $exit_code -ne 0 ]; then
     exit $exit_code
 fi
 
-python3 ./deploy/license-verification/src/job.py &
+sudo python3 ./deploy/license-verification/src/job.py &
 
-exec odoo "$@"
+sudo exec odoo "$@"
 
 
 
