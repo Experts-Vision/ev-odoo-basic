8de71747bed244d280a1ab4e67457f2a28dd6656
Kareem Shousha <kemoshousha309@gmail.com>
2025-10-11 17:40:55 +0300

    feat: add the theme init

---
+++ EV-addons/ev-theme/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "EV Theme",
+    "version": "18.0.1.0.0",
+    "category": "Theme",
+    "summary": "EV Theme",
+    "description": """
+        This Provide A custom Theme For EV
+    """,
+    "author": "Experts Vision Company",
+    "website": "https://expertsvision.org",
+    "depends": ["base", "muk_web_theme"],
+    "data": [],
+    "demo": [],
+    "application": True,
+    "license": "AGPL-3",
+    "images": ["static/description/icon.png"],
+    "support": "support@expertsvision.org",
+    'installable': True,
+}
---
+++ dependencies/muk_web_appsbar/models/res_config_settings.py
@@ -0,0 +1,16 @@
+from odoo import api, fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+
+    _inherit = 'res.config.settings'
+
+    #----------------------------------------------------------
+    # Fields
+    #----------------------------------------------------------
+    
+    appbar_image = fields.Binary(
+        related='company_id.appbar_image',
+        readonly=False
+    )
+    
