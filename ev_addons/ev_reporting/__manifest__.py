52821c306791ac42d28e4244b81c884277e18227
Kareem Shousha <kemoshousha309@gmail.com>
2025-10-26 16:38:16 +0300

    feat: init test to add a report to a po

---
+++ ev_addons/ev_reporting/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    "name": "EV Reporting",
+    "version": "18.0.1.0.0",
+    "category": "Theme",
+    "summary": "EV Reporting",
+    "description": """
+        This Provide A custom Reporting For EV
+    """,
+    "author": "Experts Vision Company",
+    "website": "https://expertsvision.org",
+    "depends": ["base", "purchase"],
+    "data": [
+        "reports/test.xml",
+        "reports/report_action.xml"
+    ],
+    "demo": [],
+    "application": False,
+    "license": "AGPL-3",
+    "images": [],
+    "support": "support@expertsvision.org",
+    'installable': True,
+}
